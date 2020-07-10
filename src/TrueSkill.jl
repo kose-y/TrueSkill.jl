44b0c092e98de212ed9d2b940378809b4b025a46
Seyoon Ko <syko0507@snu.ac.kr>
2020-07-10 21:59:46 +0900

    more exports

--- src/TrueSkill.jl
+++ src/TrueSkill.jl
@@ -1,6 +1,6 @@
 module TrueSkill
     import Base: show, isless, *, /
-    export TrueSkillEnv, Rating, expose, rate
+    export TrueSkillEnv, Rating, expose, rate, mu, sigma
     include("gaussian.jl")
     include("factorgraph.jl")
     include("distribution.jl")
